d6d14e9620db1e1700bbe2dec9c7a39a0bae967e
PeterDevLATAM <peter.devgrowth@gmail.com>
2022-03-15 17:38:10 -0500

    Basic sign in component, sign in with google pop up and with google redirect (not using redirect)

--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -4,28 +4,31 @@ import {
   signInWithRedirect,
   signInWithPopup,
   GoogleAuthProvider,
-} from 'firebase/auth';
-import { getFirestore, doc, getDoc, setDoc } from 'firebase/firestore';
+} from 'firebase/auth';//Auth
+
+import { getFirestore, doc, getDoc, setDoc } from 'firebase/firestore';//DB
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyDDU4V-_QV3M8GyhC9SVieRTDM4dbiT0Yk',
-  authDomain: 'crwn-clothing-db-98d4d.firebaseapp.com',
-  projectId: 'crwn-clothing-db-98d4d',
-  storageBucket: 'crwn-clothing-db-98d4d.appspot.com',
-  messagingSenderId: '626766232035',
-  appId: '1:626766232035:web:506621582dab103a4d08d6',
+  apiKey: "AIzaSyBLmE9OJUrr-zhvYlhH7FMb4aIsa8lps-k",
+  authDomain: "crwn-db-1c597.firebaseapp.com",
+  projectId: "crwn-db-1c597",
+  storageBucket: "crwn-db-1c597.appspot.com",
+  messagingSenderId: "643591164779",
+  appId: "1:643591164779:web:c88af4a10302cacc9f722e",
+  measurementId: "G-37VRYC7T2X"
 };
 
 const firebaseApp = initializeApp(firebaseConfig);
 
-const provider = new GoogleAuthProvider();
+const googleProvider = new GoogleAuthProvider();
 
-provider.setCustomParameters({
+googleProvider.setCustomParameters({
   prompt: 'select_account',
 });
 
 export const auth = getAuth();
-export const signInWithGooglePopup = () => signInWithPopup(auth, provider);
+export const signInWithGooglePopup = () => signInWithPopup(auth, googleProvider);
+export const signInWithGoogleRedirect = () => signInWithRedirect(auth, googleProvider);
 
 export const db = getFirestore();
 
