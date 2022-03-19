953a7590a64121a3b434e60a7893f3fc1879b3c0
PeterDevLATAM <peter.devgrowth@gmail.com>
2022-03-19 16:18:06 -0500

    handle navigate in home directory

--- src/components/directory-item/directory-item.component.jsx
+++ src/components/directory-item/directory-item.component.jsx
@@ -1,21 +1,30 @@
+import { useNavigate } from 'react-router-dom';
 import './directory-item.styles.scss';
 
 const DirectoryItem = ({ category }) => {
   const { imageUrl, title } = category;
+  const navigate = useNavigate()
+
+  const handleLink=()=>{
+    navigate(`shop/${title}`)
+  }
   return (
-    <div className='directory-item-container'>
+   
+   
+    <div className='directory-item-container' onClick={handleLink} >
       <div
         className='background-image'
         style={{
           backgroundImage: `url(${imageUrl})`,
         }}
-      />
+        />
       <div className='directory-item-body'>
         <h2>{title}</h2>
         <p>Shop Now</p>
         
       </div>
     </div>
+        
   );
 };
 
