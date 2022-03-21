90b9977b248e1ae03d617da0b9b78de6291bfd06
PeterDevLATAM <peter.devgrowth@gmail.com>
2022-03-21 15:17:02 -0500

    adedd pluging

--- client/src/components/stripe-button/stripe-button.component.jsx
+++ client/src/components/stripe-button/stripe-button.component.jsx
@@ -1,13 +1,30 @@
 import React from "react";
 import StripeCheckout from "react-stripe-checkout";
+import axios from "axios";
 
 const StripeButton = ({ price }) => {
   const priceForStripe = price * 100;
-  const publishableKey = "pk_test_TYooMQauvdEDq54NiTphI7jx";
+  const publishableKey = "pk_test_51Kfc88JohOvf1ZAJHEcaulsW82V6lSBFpqgJAigKz73smZsE1APEvSIjWOus75vWBdIcLluwWZ8RMdqFOLf7Remm00HJp033PW";
 
   const onToken = (token) => {
-    console.log(token)
-    alert('Payment Succeful')
+    console.log(token);
+    axios({
+      url: "payment",
+      method: "post",
+      data: {
+        amount: priceForStripe,
+        token,
+      },
+    })
+      .then((response) => {
+        alert("Payment succesfull");
+      })
+      .catch((error) => {
+        alert('Something went wrong with the payment, please try again latter')
+        console.log('Errorrrrrr')
+        console.log("Payment Error: ", JSON.parse(error));
+        
+      });
   };
   return (
     <StripeCheckout
